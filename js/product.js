ce5424319f533e888b0d8f88e28e8ee680404d7e
ducanhptitb19cn028 <99241227+ducanhptitb19cn028@users.noreply.github.com>
2023-04-12 08:26:18 +0700

    commit

--- js/product.js
+++ js/product.js
@@ -23,3 +23,38 @@ function deleteProductbyId(id){
     xhr.send();
 }
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
+//paste here your copied configuration code
+const firebaseConfig = {
+    apiKey: "AIzaSyCrhXy6MadW640N6mB-e3cGmL5u3s8aL6s",
+    authDomain: "hdvnhom8.firebaseapp.com",
+    databaseURL: "https://hdvnhom8-default-rtdb.firebaseio.com",
+    projectId: "hdvnhom8",
+    storageBucket: "hdvnhom8.appspot.com",
+    messagingSenderId: "321095979248",
+    appId: "1:321095979248:web:fa7f40b6b5eda17e9af235",
+    measurementId: "G-REPQT5RB2Z"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase);
+function upImage() {
+    alert("Button clicked!");
+    const ref = firebase.storage().ref();
+    const file = document.querySelector("#photo").files[0];
+    const name = + new Date() + "-" + file.name;
+    const metadata = {
+        contentType: file.type
+    };
+    const task = ref.child(name).put(file, metadata);
+    task.then(snapshot => snapshot.ref.getDownloadURL())
+    .then(url => {
+        console.log(url);
+        alert('image uploaded successfully');
+        document.querySelector("#image").src = url;
+    })
+    .catch(console.error);
+}
+const errorMsgElement = document.querySelector('span#errorMsg');
