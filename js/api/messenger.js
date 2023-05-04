7d91981ed4eb261ea47cc8178583f03348e710cf
ducanhptitb19cn028 <99241227+ducanhptitb19cn028@users.noreply.github.com>
2023-05-04 11:26:19 +0700

    test mess fail

--- js/api/messenger.js
+++ js/api/messenger.js
@@ -1,7 +1,13 @@
 const chatBtn = document.getElementById('chat-btn');
 
 chatBtn.addEventListener('click', () => {
-    fetch('http://localhost:8080/api/v1/messenger-chat')
+    fetch('http://localhost:8080/api/v1/messenger-chat', {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      })
         .then(response => {
             if (response.ok) {
                 window.location.href = response.url;
